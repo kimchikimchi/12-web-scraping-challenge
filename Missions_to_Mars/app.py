5612de608ce50662c20de7faddba2edadf4a02cb
Jay Cha <kimchikimchi@gmail.com>
2021-04-25 18:23:41 -0400

    - Added bootstrap element to the template html - Fixed and populated route routines in Flask app

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -1,6 +1,7 @@
 # Import libraries
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
+from pprint import pprint
 
 import scrape_mars
 
@@ -11,26 +12,25 @@ app = Flask(__name__)
 mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_db")
 mars  = mongo.db.mars
 
-# Drop collection if available to remove dups
-mars.drop()
-
 # Routes
 
 # Create a root route / that will query your Mongo database and pass the mars data into an HTML template to display the data.
 @app.route('/')
 def index():
-    mars_data = mars.find()
-    # return render_template("index.html", mars_data=mars_data)
-    return render_template("index.html")
+    mars_data = mars.find_one()
+    return render_template("index.html", mars_data=mars_data)
 
 # Create a route called /scrape that will import your scrape_mars.py script and call your scrape function.
 @app.route('/scrape')
 def scrape():
+    # Drop collection if available to remove dups
+    mars.drop()
+
     # Collect data from websites
-    data = scrape_mars.scrape()
+    mars_data = scrape_mars.scrape()
 
     # Update the Mongo database using update and upsert=True
-    mars.update({}, data, upsert=True)
+    mars.update({}, mars_data, upsert=True)
     return redirect("/", code=302)
 
 
